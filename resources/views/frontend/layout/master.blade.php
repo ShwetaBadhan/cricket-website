e5eb82dc5316d32f96f234e59e88055a02dd2bf4
gracychadha <chadhagracy8@gmail.com>
2026-03-11 13:38:14 +0530

    WP Integration

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -74,5 +74,6 @@
 <script type="text/javascript" src="{{url ('assets/js/rev-slider/js/extensions/revolution.extension.parallax.min.js')}}"></script> 
 <script type="text/javascript" src="{{url ('assets/js/rev-slider/js/extensions/revolution.extension.slideanims.min.js')}}"></script> 
 <script type="text/javascript" src="{{url ('assets/js/rev-slider/js/extensions/revolution.extension.video.min.js')}}"></script>
+@stack('scripts')
 </body>
 </html>
--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -16,32 +16,71 @@
     @include('frontend.components.home.features')
 
     {{-- banner --}}
-    {{-- <div class="banner-wrap text-center wf100 mb-80"> <img src="{{url('assets/images/placeyourbanner.png')}}" alt=""> --}}
-    {{-- widgets --}}
-    @include('frontend.components.home.widgets')
-    {{-- work flow --}}
-    @include('frontend.components.home.work-flow')
-    
-    <!--Banner Size 920 x 100 End-->
-    {{-- news --}}
-    @include('frontend.components.home.news')
+    {{-- <div class="banner-wrap text-center wf100 mb-80"> <img src="{{url('assets/images/placeyourbanner.png')}}" alt="">
+      --}}
+      {{-- widgets --}}
+      @include('frontend.components.home.widgets')
+      {{-- work flow --}}
+      @include('frontend.components.home.work-flow')
 
-    {{-- team --}}
-    @include('frontend.components.home.team')
+      <!--Banner Size 920 x 100 End-->
+      {{-- news --}}
+      @include('frontend.components.home.news')
 
-    
+      {{-- team --}}
+      @include('frontend.components.home.team')
 
-    {{-- gallery --}}
-    @include('frontend.components.home.gallery')
 
-  
 
-    {{-- sponsors --}}
-    @include('frontend.components.home.sponsors')
+      {{-- gallery --}}
+      @include('frontend.components.home.gallery')
 
-    {{-- tweets --}}
-    @include('frontend.components.home.tweets')
-  </div>
-  <!--Main Content End-->
+
+
+      {{-- sponsors --}}
+      @include('frontend.components.home.sponsors')
+
+      {{-- tweets --}}
+      @include('frontend.components.home.tweets')
+    </div>
+    <!--Main Content End-->
 
 @endsection
+  @push('scripts')
+    <script>
+      var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?86687';
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = url;
+      var options = {
+        "enabled": true,
+        "chatButtonSetting": {
+          "backgroundColor": "#2ACA45;",
+          "ctaText": "",
+          "borderRadius": "25",
+          "marginLeft": "20",
+          "marginBottom": "30",
+          "marginRight": "50",
+          "position": "left"
+        },
+        "brandSetting": {
+          "brandName": "Jharkhand Super League",
+          "brandSubTitle": "Typically replies within a day",
+          "brandImg": "../assets/images/logo/fav.png",
+          "welcomeText": "Hi there!\nHow can I help you?",
+          "messageText": "Hello, I have a question about ",
+          "backgroundColor": "#2ACA45;",
+          "ctaText": "Start Chat",
+          "borderRadius": "25",
+          "autoShow": false,
+          "phoneNumber": "+919263747143"
+        }
+      };
+      s.onload = function () {
+        CreateWhatsappChatWidget(options);
+      };
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    </script>
+  @endpush
